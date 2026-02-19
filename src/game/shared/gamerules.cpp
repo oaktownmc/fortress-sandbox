4bb1a7dced644e505dcdea0a2cc346aec3d722cc
lod carpshote <headless165@gmail.com>
2026-02-19 22:23:46 +0100

    increased fov limit to 140 + changed title to fortress sandbox

--- src/game/shared/gamerules.cpp
+++ src/game/shared/gamerules.cpp
@@ -870,7 +870,7 @@ void CGameRules::ClientSettingsChanged( CBasePlayer *pPlayer )
 	if ( pszFov )
 	{
 		int iFov = atoi(pszFov);
-		iFov = clamp( iFov, 75, 90 );
+		iFov = clamp( iFov, 75, MAX_FOV );
 		pPlayer->SetDefaultFOV( iFov );
 	}
 
